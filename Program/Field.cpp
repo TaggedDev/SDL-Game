612d6468a3746c678c426ae6e58347998adac146
SNL-Kimchi <dr.fphka@yandex.ru>
2021-04-05 10:43:22 +0300

    Update Field.cpp

--- Program/Field.cpp
+++ Program/Field.cpp
@@ -2,4 +2,6 @@
 
 void Field::Render()
 {
+	//     <IncludePath>C:\Users\User\Documents\Easy c++\SDL1\SDL2-devel-2.0.14-VC\SDL2-2.0.14\include;$(IncludePath)</IncludePath>
+	//<LibraryPath>C:\Users\denis\Desktop\SDL2 - devel - 2.0.14 - VC\SDL2 - 2.0.14\lib\x64; C:\Users\User\Documents\Easy c++\SDL1\SDL2 - devel - 2.0.14 - VC\SDL2 - 2.0.14\lib\x64; $(LibraryPath) < / LibraryPath >
 }
