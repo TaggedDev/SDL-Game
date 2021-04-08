4010551a1c8dacbcbfb28bbd0dcaea69872ee14d
SNL-Kimchi <dr.fphka@yandex.ru>
2021-04-08 20:00:50 +0300

    [PLAYER] movement update

--- Program/Player.cpp
+++ Program/Player.cpp
@@ -28,49 +28,45 @@ void Player::move_right(SDL_Renderer* ren)
 {
     int save = x;
     if (x + 50 < 1081) {
-        while (x < save + 5) {
+        while (x < save + 100) {
             full_rect_model(ren, x, y, 50, 25);
             SDL_RenderPresent(ren);
             full_rect_bckg(ren, x, y, 50, 25);
             x++;
         }
-        x += 5;
     }
 }
 void Player::move_down(SDL_Renderer* ren)
 {
     int save1 = y;
     if (y + 25 < 501) {
-        while (y < save1 + 5) {
+        while (y < save1 + 100) {
             full_rect_model(ren, x, y, 50, 25);
             SDL_RenderPresent(ren);
             full_rect_bckg(ren, x, y, 50, 25);
             y++;
         }
-        y += 5;
     }
 }
 void Player::move_left(SDL_Renderer* ren) {
-    int save1 = x;
+    int save2 = x;
     if (x > 0) {
-        while (x > save1 - 5) {
+        while (x > save2 - 100) {
             full_rect_model(ren, x, y, 50, 25);
             SDL_RenderPresent(ren);
             full_rect_bckg(ren, x, y, 50, 25);
             x--;
         }
-        x -= 5;
     }
 }
 void Player::move_up(SDL_Renderer* ren) {
-    int save1 = y;
+    int save3 = y;
     if (y > 0) {
-        while (y > save1 - 5) {
+        while (y > save3 - 100) {
             full_rect_model(ren, x, y, 50, 25);
             SDL_RenderPresent(ren);
             full_rect_bckg(ren, x, y, 50, 25);
             y--;
         }
-        y -= 5;
     }
 }
