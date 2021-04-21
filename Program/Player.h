16505d70fcde57be426733444a4f7051ee274ec7
TaggedDev <45800215+TaggedDev@users.noreply.github.com>
2021-04-21 18:16:49 +0300

    [Players] Recreated drawing function with updating square vector array

--- Program/Player.h
+++ Program/Player.h
@@ -3,6 +3,7 @@
 #include "Player.h"
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Player
@@ -12,6 +13,7 @@ public:
 	void move_down(SDL_Renderer* ren);
 	void move_up(SDL_Renderer* ren);
 	void move_left(SDL_Renderer* ren);
+	void GetSurface(SDL_Texture*& tex, SDL_Renderer* ren, SDL_Surface* surf, vector<vector<SDL_Color>>& pixelarray);
 	void draw(SDL_Renderer* ren, int factor, int x, int y);
 	void shooting(SDL_Renderer* ren, Player player1, Player player2);
 	int x = 300;
@@ -19,10 +21,15 @@ public:
 	int state = 1;
 	bool isDead = false;
 	string color;
+	SDL_Texture* tex;
+	vector<vector<SDL_Color>> pixelarray;
+	//SDL_Surface* sur;
 	Player(int inputX, int inputY, string inputColor, SDL_Renderer* render) {
+		vector<vector<SDL_Color>> creater(60, vector<SDL_Color>(60));
 		x = inputX;
 		y = inputY;
 		color = inputColor;
+		pixelarray = creater;
 	}
 	// state
 	// 1 - up
--- Program/PlayerRender.cpp
+++ Program/PlayerRender.cpp
@@ -4,6 +4,61 @@
 
 using namespace std;
 
+Uint32 getpixel(SDL_Surface* surface, int x, int y)
+{
+    int bpp = surface->format->BytesPerPixel;
+    /* Here p is the address to the pixel we want to retrieve */
+    Uint8* p = (Uint8*)surface->pixels + y * surface->pitch + x * bpp;
+
+    switch (bpp)
+    {
+    case 1:
+        return *p;
+        break;
+
+    case 2:
+        return *(Uint16*)p;
+        break;
+
+    case 3:
+        if (SDL_BYTEORDER == SDL_BIG_ENDIAN)
+            return p[0] << 16 | p[1] << 8 | p[2];
+        else
+            return p[0] | p[1] << 8 | p[2] << 16;
+        break;
+
+    case 4:
+        return *(Uint32*)p;
+        break;
+
+    default:
+        return 0;       /* shouldn't happen, but avoids warnings */
+    }
+}
+
+void Player::GetSurface(SDL_Texture*& tex, SDL_Renderer* ren, SDL_Surface* surf, vector<vector<SDL_Color>>& pixelarray) {
+    SDL_Color rgb;
+    for (int i = 0; i < 60; i++)
+    {
+        for (int j = 0; j < 60; j++)
+        {
+            Uint32 data = getpixel(surf, i, j);
+            SDL_GetRGB(data, surf->format, &rgb.r, &rgb.g, &rgb.b);
+            pixelarray[i][j] = rgb;
+        }
+    }
+}
+
+void DrawSurface(SDL_Renderer* ren, vector<vector<SDL_Color>> pixelarray, int newx, int newy) {
+    for (int i = 0; i < 60; i++)
+    {
+        for (int j = 0; j < 60; j++)
+        {
+            SDL_SetRenderDrawColor(ren, pixelarray[i][j].r, pixelarray[i][j].g, pixelarray[i][j].b, pixelarray[i][j].a);
+            SDL_RenderDrawPoint(ren, i + newx, j + newy);
+        }
+    }
+}
 
 void applySurface(int x, int y, SDL_Texture* tex, SDL_Renderer* ren) {
     SDL_Rect pos;
@@ -26,9 +81,8 @@ void Player::draw(SDL_Renderer* ren, int factor, int x, int y)
     else if (factor == 4)
         surface = SDL_LoadBMP("src\\downbluetank.bmp");
 
-    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, surface);
+    GetSurface(tex, ren, surface, pixelarray);
+    DrawSurface(ren, pixelarray, x, y);
     SDL_FreeSurface(surface);
-
-    applySurface(x, y, tex, ren);
     SDL_RenderPresent(ren);
 }
